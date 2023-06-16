d0b63bf3a1db1f7671b3bcf39ac20fd0472229c1
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-16 11:17:13 +0200

    Country: add a function addTeam() and displayTeam(), this function will display all team of a contry

--- Country.php
+++ Country.php
@@ -2,17 +2,29 @@
 
 class Country{
     private string $_contryName;
-
+    private array $_team;
+    
     public function __construct(string $contryName){
         $this->_contryName = $contryName;
-
+        $this->_team= [];
     }
 
+    // $this->_team->getCountry()->addPlay($this);
     public function getContryName() :string{
         return $this->_contryName;
     }    
     public function setContryName($contryName){
         $this->_contryName = $contryName;
+    }
+
+    public function addTeam(Team $team){
+        $this->_team[] = $team;
+    }
+
+    public function displayTeam(){
+        foreach($this->_team as $team){
+            return $team;
+        }
     }
 
     public function __toString(){
